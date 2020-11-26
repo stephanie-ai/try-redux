d6a2125b500c2119ea3af05a1881766b9349156d
stephanie-ai <stephanie_cooper@hotmail.com>
2020-11-26 17:48:48 +0000

    add CatCard with like button

---
+++ src/components/CatCard/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { LikeButton } from './LikeButton';
+import { toggleLikeCat } from '../actionCreators/CatActions';
+
+class CatCard extends Component {
+    handleToggleLike = id => {
+        this.props.toggleLikeCat(id);
+    }    
+
+    render() {
+        const { id, img, liked } = this.props.cat
+
+        return (
+            <div className="card">
+                <img src={img} />
+                <LikeButton liked={liked} handleClick={() => this.handleToggleLike(id)}/>
+            </div>
+        )
+    }
+}
+
+export default connect(null, { toggleLikeCat })(CatCard);
